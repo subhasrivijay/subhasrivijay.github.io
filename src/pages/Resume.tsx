f471cdda44db3ecb96b1233347c3686605c4a8ea
subhasrivijay <svijay2@jh.edu>
2025-07-05 19:26:20 -0500

    Resume

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,5 @@
 import BackToHomeButton from "../components/BackToHomeButton";
-const resumePdfUrl = "/RESUME_SUBHASRI_VIJAY.pdf"; // Place your PDF in /
+const resumePdfUrl = `${import.meta.env.BASE_URL}RESUME_SUBHASRI_VIJAY.pdf`;
 
 export default function Resume() {
   return (
